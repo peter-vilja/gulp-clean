d6e0ef8d72e1aecbf4e1adb437f1b1e715e4841e
Peter Vilja <peter.vilja@gmail.com>
2014-05-25 12:33:09 +0300

    refactor to emit errors and push files

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 'use strict';
 var rimraf = require('rimraf');
-var through = require('through2').obj;
+var through2 = require('through2');
 var gutil = require('gulp-util');
 var path = require('path');
 
 module.exports = function (options) {
-  return through(function (file, _, cb) {
+  return through2.obj(function (file, enc, cb) {
     // Paths are resolved by gulp
     var filepath = file.path;
     var cwd = file.cwd;
@@ -14,18 +14,24 @@ module.exports = function (options) {
     // Prevent mistakes with paths
     if (!(relative.substr(0, 2) === '..') && relative !== '' || (options ? (options.force && typeof options.force === 'boolean') : false)) {
       rimraf(filepath, function (error) {
-        if (!error) {
-          return cb(null, file);
-        } else {
-          return cb(new Error('Unable to delete "' + filepath + '" file (' + error.message + ').'), file);
+        if (error) {
+          this.emit('error', new gutil.PluginError('gulp-clean', 'Unable to delete "' + filepath + '" file (' + error.message + ').'));
         }
-      });
+        this.push(file);
+        cb();
+      }.bind(this));
     } else if (relative === '') {
-      gutil.log('gulp-clean: Cannot delete current working directory. (' + filepath + ')');
-      return cb(null, file);
+      var msgCurrent = 'Cannot delete current working directory. (' + filepath + '). Use option force.';
+      gutil.log('gulp-clean: ' + msgCurrent);
+      this.emit('error', new gutil.PluginError('gulp-clean', msgCurrent));
+      this.push(file);
+      cb();
     } else {
-      gutil.log('gulp-clean: Cannot delete files outside the current working directory. (' + filepath + ')');
-      return cb(null, file);
+      var msgOutside = 'Cannot delete files outside the current working directory. (' + filepath + '). Use option force.';
+      gutil.log('gulp-clean: ' + msgOutside);
+      this.emit('error', new gutil.PluginError('gulp-clean', msgOutside));
+      this.push(file);
+      cb();
     }
   });
 };
